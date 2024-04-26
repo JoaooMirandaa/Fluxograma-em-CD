a53e93f57946781f7cfad1d9ddd8779a7c3c645d
JoaooMirandaa <164816713+JoaooMirandaa@users.noreply.github.com>
2024-04-26 17:28:20 -0300

    Add files via upload

---
+++ diagrama-de-classes-em-CD/src/Vendedor.java
@@ -0,0 +1,31 @@
+public class Vendedor {
+
+
+
+
+    private String nome;
+    private String sexo;
+    private String numero;
+    private String cpf;
+
+    //metodos
+
+    void registrarCompra(){
+
+
+    }
+
+    void receberPagamento(){
+
+
+    }
+
+    void emitirNotaFiscal(){
+
+
+    }
+
+    void consultarProdutosDisponiveis(){
+        
+    }
+}
